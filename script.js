579cfbe5dc8aa86d979ecb6ff37a6f9d0c829372
Witcorpindiaadvisorllp <167207085+Witcorpindiaadvisorllp@users.noreply.github.com>
2026-05-19 16:07:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -868,10 +868,6 @@ function searchDSC(query) {
 
     renderDSC(filtered);
 }
-
-    </script>
-          
-    <script>
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(() => console.log("SW registered"));
