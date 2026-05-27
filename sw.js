dd760dbff04348297b8d31e301cd9d2ceb32fd32
Witcorpindiaadvisorllp <167207085+Witcorpindiaadvisorllp@users.noreply.github.com>
2026-05-27 17:53:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
 self.addEventListener('install', event => {
   console.log("Service Worker Installed");
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener('activate', event => {
+  console.log("Service Worker Activated");
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response("Offline mode: No internet");
+    })
+  );
+});
